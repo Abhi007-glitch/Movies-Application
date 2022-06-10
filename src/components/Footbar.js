45fc544e5fd978dc46b1fd9cf5f05dd5083a8816
Abhinav Sharma <64996545+Abhi007-glitch@users.noreply.github.com>
2022-06-11 02:39:13 +0530

    complete upload

---
+++ src/components/Footbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footbar() {
+  return (
+    <div className='"flex flex-col min-h-screen "'>
+    <footer >
+    <div className="text-center p-4   background-color: rgba(0, 0, 0, 0.2)  ; ">
+    © 2022 Copyright:
+    <span class="text-whitehite" > AJ9U</span>
+  </div>
+</footer>
+</div>
+   
+  )
+}
+
+export default Footbar
